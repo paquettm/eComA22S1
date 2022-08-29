d52013b1d0d505b1eb40dcbb306e38d8631f8a41
Michel Paquette <paquettm@vaniercollege.qc.ca>
2022-08-29 11:50:51 -0400

    added routing

---
+++ app/controllers/controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\controllers;
+
+class controller{
+
+	public function index2(){
+		echo "hi";
+	}
+}
--- index.php
+++ index.php
@@ -3,10 +3,5 @@
 	// include_once() try to include (if not previously included) and warn on failure
 	// require() try to include and halt on failure
 	// require_once() try to include (if not previously included) and halt on failure
-	if(!isset($_GET['url'])){
-		header('location:/');
-		exit();
-	}
-
 	require("app/core/init.php");
 	new app\core\App();
