aca2b73294b4ccd5464927949d608302ef5ca98b
Michel Paquette <paquettm@vaniercollege.qc.ca>
2022-09-16 12:50:37 -0400

    Added Model base class, added Owner Model and DB export.

---
+++ app/views/Vet/addOwner.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Some title</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<form action='' method='post'>
+	<label>First name:<input type="text" name="first_name" /></label><br>
+	<label>Last name:<input type="text" name="last_name" /></label><br>
+	<label>Contact:<input type="text" name="contact" /></label><br>
+	<input type="submit" name="action" value="Add owner" />
+</form>
+
+</body>
+</html>
