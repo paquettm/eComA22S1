661022407bbff0542932ebae21a10adb0975f5bd
Michel Paquette <paquettm@vaniercollege.qc.ca>
2022-09-29 12:49:50 -0400

    DB file

--- vet_clinic.sql
+++ vet_clinic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2022 at 06:40 PM
+-- Generation Time: Sep 29, 2022 at 06:48 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -43,8 +43,7 @@ CREATE TABLE `animal` (
 --
 
 INSERT INTO `animal` (`animal_id`, `owner_id`, `name`, `profile_pic`, `dob`) VALUES
-(6, 4, '', '', '0000-00-00'),
-(7, 4, 'Mister Mario', '632c8de4a9beb.png', '2022-09-19');
+(9, 4, 'Cute Kitten', '6335ab5d5c77d.jpg', '2022-09-28');
 
 -- --------------------------------------------------------
 
@@ -67,6 +66,28 @@ CREATE TABLE `owner` (
 INSERT INTO `owner` (`owner_id`, `first_name`, `last_name`, `contact`) VALUES
 (4, 'Mister', 'Blue', '514-555-5555');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password_hash` varchar(63) NOT NULL,
+  `role` enum('user','admin') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password_hash`, `role`) VALUES
+(1, 'Tarzan', '$2y$10$HuEeD9YiMeh6V1OmVFCKsuKjupjEJp1Hr5KRekkxoVl300WPUOFuq', 'admin'),
+(3, 'Jane', '$2y$10$bH1LWlVNkXM18the5Naf1OasgLqCgE3kcV81SQdKZD/D/K0/sfDvi', 'user');
+
 --
 -- Indexes for dumped tables
 --
@@ -85,6 +106,13 @@ ALTER TABLE `owner`
   ADD PRIMARY KEY (`owner_id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -92,13 +120,19 @@ ALTER TABLE `owner`
 -- AUTO_INCREMENT for table `animal`
 --
 ALTER TABLE `animal`
-  MODIFY `animal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `animal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `owner`
 --
 ALTER TABLE `owner`
   MODIFY `owner_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
